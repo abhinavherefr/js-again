39b1bc5cc8c53e871f6a17f69c5abbd9cd685a49
Abhinav Sharma <93465800+abhinavherefr@users.noreply.github.com>
2024-08-26 13:11:25 +0000

    Datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 341241;
+let accountEmail = "whatevere@yehoo.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+
+accountId = 2;
+console.log(accountId);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+
+// alert("Hello!") WONT WORK ON NODE
+
+console.log(3 + 3)
+
+console.log("What the heck");
+
+
+// number ki range=> 2 ^ 53 
+// bigInt =>  
+// string =>
+// boolean =>
+// null => standalone value  UNDEFINED NAHI HAI KHALI HAI
+// undefined
+
+// symbol => uniqueness 
+
+// object
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //Undefined
+
